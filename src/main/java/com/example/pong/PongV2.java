13654d400600a01d6210043c40fbbc334d1b53ef
NColu20020 <98605883+NColu20020@users.noreply.github.com>
2022-04-11 09:16:17 +0200

    new stuff

--- src/main/java/com/example/pong/PongV2.java
+++ src/main/java/com/example/pong/PongV2.java
@@ -16,74 +16,86 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * Class defines Nicolas and Stephen's version of the classic PONG GAME
+ * CS2040 Final Project
+ */
 public class PongV2 extends Application {
-    private static final int WIDTH= 800;
+    private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //WELCOME SCREEN
-        //create the window widgets
-        Text welcome = new Text(WIDTH/2-100, HEIGHT/3, "Welcome to PONG!");
-        welcome.setFont(Font.font("Verdana", 20));
-
-        Button start = new Button("Start Game");
-        start.setLayoutX(WIDTH/2-40);
-        start.setLayoutY(HEIGHT/2);
-        start.setScaleX(1.5);
-        start.setScaleY(1.5);
-
-        Text instructions = new Text(WIDTH/2-40,HEIGHT-100,"Instructions");
-
-        //create the panes
+        //Declares the 3 panes, one for each screen
         Pane p1 = new Pane();
         Pane p2 = new Pane();
         Pane p3 = new Pane();
 
-
-        // define the pane hierarchy
-        p1.getChildren().addAll(welcome, start, instructions);
-
-        //create a new scene with parent node
+        //Declare the 3 screens of the game
         Scene welcomeScreen = new Scene(p1, WIDTH, HEIGHT);
         Scene gameScreen = new Scene(p2, WIDTH, HEIGHT);
         Scene overScreen = new Scene(p3, WIDTH, HEIGHT);
 
-        //GAME SCREEN
-        start.setOnMouseClicked(e->{
+        //WELCOME SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //create the window widgets of the WELCOME SCREEN
+        Text welcome = new Text(WIDTH / 2 - 100, HEIGHT / 3, "Welcome to PONG!");
+        welcome.setFont(Font.font("Verdana", 20));
+
+        Button start = new Button("Start Game");
+        start.setLayoutX(WIDTH / 2 - 40);
+        start.setLayoutY(HEIGHT / 2);
+        start.setScaleX(1.5);
+        start.setScaleY(1.5);
+
+        Text instructions = new Text(WIDTH / 2 - 40, HEIGHT - 100, "Instructions");
+
+        // define the pane hierarchy for the WELCOME SCREEN
+        p1.getChildren().addAll(welcome, start, instructions);
+
+        //EVENT Listeners for WELCOME SCREEN
+        start.setOnMouseClicked(e -> {
             primaryStage.setScene(gameScreen);
         });
 
-        Rectangle humanPaddle = new Rectangle (20,20, 15,150);
+        //GAME SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //create the window widgets of the GAME SCREEN
+        Rectangle humanPaddle = new Rectangle(20, 20, 15, 150);
 
-        Rectangle computerPaddle = new Rectangle (550,20,150,15);
-        Circle ball = new Circle(500,500,15);
+        Rectangle computerPaddle = new Rectangle(550, 20, 150, 15);
+        Circle ball = new Circle(500, 500, 15);
         Button quit = new Button("QUIT");
-        quit.setLayoutX(WIDTH/2-40);
-        quit.setLayoutY(HEIGHT/2);
-        quit.setOnMouseClicked(e->{
+        quit.setLayoutX(WIDTH / 2 - 40);
+        quit.setLayoutY(HEIGHT / 2);
+
+        // defines the pane hierarchy for the WELCOME SCREEN
+        p2.getChildren().addAll(humanPaddle, computerPaddle, ball, quit);
+
+        //EVENT Listeners for GAME SCREEN
+        quit.setOnMouseClicked(e -> {
             primaryStage.setScene(overScreen);
         });
 
-        p2.getChildren().addAll(humanPaddle, computerPaddle,ball,quit);
-
-        //GAME OVER SCREEN
-
+        //GAME OVER SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //create the window widgets of the GAME OVER SCREEN
         Button quit2 = new Button("QUIT");
-        quit2.setLayoutX(WIDTH/2-40);
-        quit2.setLayoutY(HEIGHT/2);
+        quit2.setLayoutX(WIDTH / 2 - 40);
+        quit2.setLayoutY(HEIGHT / 2);
         Button restart = new Button("RESTART");
         Text gameOver = new Text("GAME OVER !");
 
-        quit2.setOnMouseClicked(e->{
+        //Defines the pane hierarchy for the GAME OVER SCREEN
+        p3.getChildren().addAll(quit2, restart, gameOver);
+
+        //Event listeners for GAME OVER SCREEN
+        quit2.setOnMouseClicked(e -> {
             primaryStage.setScene(welcomeScreen);
         });
-        restart.setOnMouseClicked(e->{
+        restart.setOnMouseClicked(e -> {
             primaryStage.setScene(gameScreen);
         });
 
-        p3.getChildren().addAll(quit2,restart,gameOver);
-
-        //show the window
+       // --------------------------------------------------------------------------------------------------------------------------------------
+        //shows the window of the application, begining with the WELCOME SCREEN
         primaryStage.setTitle("P O N G");
         primaryStage.setScene(welcomeScreen);
         primaryStage.show();
