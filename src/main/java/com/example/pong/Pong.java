8820292d54da89c8d18075c89a9164a83648d993
NColu20020 <98605883+NColu20020@users.noreply.github.com>
2022-04-22 18:38:48 +0200

    Updates by Nick

--- src/main/java/com/example/pong/Pong.java
+++ src/main/java/com/example/pong/Pong.java
@@ -1,131 +1,380 @@
 package com.example.pong;
 
-import java.util.Random;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.Interpolator;
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.*;
 import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-//Can you see this ?
-//Test again
-public class Pong extends Application {
 
-    //variable
-    private static final int width = 800;
-    private static final int height = 600;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Class defines Nicolas and Stephen's version of the classic PONG GAME
+ * CS2040 Final Project
+ */
+public class Pong extends Application {
+    //Global variable declarations
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
     private static final int PLAYER_HEIGHT = 100;
     private static final int PLAYER_WIDTH = 15;
-    private static final double BALL_R = 15;
-    private int ballYSpeed = 1;
-    private int ballXSpeed = 1;
-    private double playerOneYPos = height / 2;
-    private double playerTwoYPos = height / 2;
-    private double ballXPos = width / 2;
-    private double ballYPos = height / 2;
-    private int scoreP1 = 0;
-    private int scoreP2 = 0;
-    private boolean gameStarted;
-    private int playerOneXPos = 0;
-    private double playerTwoXPos = width - PLAYER_WIDTH;
+    private static final double BALL_RADIUS = 15;
+    private int playerScore = 0;
+    private int lives = 3;
+    private static List<Players> scoreBoard = new ArrayList<Players>();//arrayList to store scores for scoreboards
 
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("P O N G");
-        //background size
-        Canvas canvas = new Canvas(width, height);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+    //Global node declarations
+    //create the window widgets of the GAME SCREEN
+    Rectangle humanPaddle = new Rectangle(0, 20, PLAYER_WIDTH, PLAYER_HEIGHT);
+    Rectangle computerPaddle = new Rectangle(785, (HEIGHT / 2 - 75), PLAYER_WIDTH, PLAYER_HEIGHT);
+    Circle ball = new Circle(0, 0, BALL_RADIUS);
+    Label score = new Label("" + playerScore);
+    Label currentLives = new Label("" + lives);
 
-        //JavaFX Timeline = free form animation defined by KeyFrames and their duration
-        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
-        //number of cycles in animation INDEFINITE = repeat indefinitely
-        tl.setCycleCount(Timeline.INDEFINITE);
+    //Global window widgets of the GAME OVER SCREEN
+    Label finalScore = new Label("" + playerScore);
 
-        //mouse control (move and click)
-        canvas.setOnMouseMoved(e ->  playerOneYPos  = e.getY());
-        canvas.setOnMouseClicked(e ->  gameStarted = true);
-        stage.setScene(new Scene(new StackPane(canvas)));
-        stage.show();
-        tl.play();
-    }
+    //Random object global declaration
+    Random rand = new Random();
 
-    private void run(GraphicsContext gc) {
-        //set graphics
-        //set background color
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, width, height);
+    // New Table View
+    TableView tbv = new TableView();
 
-        //set text
-        gc.setFill(Color.WHITE);
-        gc.setFont(Font.font(25));
+    //Declares the 3 panes, one for each screen
+    Pane p1 = new Pane();
+    Pane p2 = new Pane();
+    Pane p3 = new Pane();
 
-        if(gameStarted) {
-            //set ball movement
-            ballXPos+=ballXSpeed;
-            ballYPos+=ballYSpeed;
+    //Declare the 3 screens of the game
+    Scene welcomeScreen = new Scene(p1, WIDTH, HEIGHT);
+    Scene gameScreen = new Scene(p2, WIDTH, HEIGHT);
+    Scene overScreen = new Scene(p3, WIDTH, HEIGHT);
 
-            //simple computer opponent who is following the ball
-            if(ballXPos < width - width  / 4) {
-                playerTwoYPos = ballYPos - PLAYER_HEIGHT / 2;
-            }  else {
-                playerTwoYPos =  ballYPos > playerTwoYPos + PLAYER_HEIGHT / 2 ?playerTwoYPos += 1: playerTwoYPos - 1;
+    //Ball animation declarations
+    PathTransition ptBall = new PathTransition();
+
+    //Stage declaration
+    Stage primaryStage = new Stage();
+
+    @Override
+    /**
+     * Defines the game windows and nodes + some game logic
+     */
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        //WELCOME SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //create the window widgets of the WELCOME SCREEN
+        Text welcome = new Text(WIDTH / 2 - 100, HEIGHT / 3, "Welcome to PONG!");
+        welcome.setFont(Font.font("Verdana", 20));
+
+        Button start = new Button("Start Game");
+        start.setLayoutX(WIDTH / 2 - 40);
+        start.setLayoutY(HEIGHT / 2);
+        start.setScaleX(1.5);
+        start.setScaleY(1.5);
+
+        Text instructions = new Text(WIDTH / 2 - 40, HEIGHT - 100, "Instructions");
+
+        // define the pane hierarchy for the WELCOME SCREEN
+        p1.getChildren().addAll(welcome, start, instructions);
+
+        //EVENT Listeners for WELCOME SCREEN
+        start.setOnMouseClicked(e -> {
+            primaryStage.setScene(gameScreen);
+            gamePlay();
+
+        });
+
+        //GAME SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //create the window widgets of the GAME SCREEN
+        Button quit = new Button("QUIT");
+        quit.setLayoutX(WIDTH - 50);
+        quit.setLayoutY(10);
+
+        Text scoreLabel = new Text("Player Score:");
+        scoreLabel.setLayoutX(WIDTH / 2 - 30);
+        scoreLabel.setLayoutY(12);
+
+        //Score label
+        score.setLayoutX(WIDTH / 2);
+        score.setLayoutY(20);
+
+        Text livesLabel = new Text("Lives Left:");
+        livesLabel.setLayoutX(WIDTH / 2 - 20);
+        livesLabel.setLayoutY(570);
+
+        //Score label
+        currentLives.setLayoutX(WIDTH / 2);
+        currentLives.setLayoutY(580);
+
+        // defines the pane hierarchy for the WELCOME SCREEN
+        p2.getChildren().addAll(humanPaddle, computerPaddle, ball, quit, scoreLabel, score, currentLives, livesLabel);
+
+        //EVENT Listeners for GAME SCREEN
+        quit.setOnMouseClicked(e -> {
+            finalScore.setText("" + playerScore);
+            primaryStage.setScene(overScreen);
+            ptBall.stop();
+        });
+
+//        gameScreen.setOnKeyPressed(e -> {
+//            switch (e.getCode()) {
+//                case DOWN:
+//                    humanPaddle.setY(humanPaddle.getY() + 10);
+//                    break;
+//                case UP:
+//                    humanPaddle.setY(humanPaddle.getY() - 10);
+//                    break;
+//            }
+//        });
+
+        gameScreen.setOnMouseMoved(e -> {
+            humanPaddle.setY(e.getY());
+            //System.out.printf("py: %f, px: %f, by: %f, bx: %f \n", humanPaddle.getY(),humanPaddle.getX(),ball.getTranslateY(),ball.getTranslateX());
+
+        });
+
+        ball.translateYProperty().addListener(ov ->
+        {
+            computerPaddle.setY((ball.getTranslateY() - PLAYER_HEIGHT / 2));
+            if (ball.getTranslateY() >= HEIGHT || ball.getTranslateY() <= 0) {
+                ptBall.setRate(ptBall.getRate() * -1);
             }
-            //draw the ball
-            gc.fillOval(ballXPos, ballYPos, BALL_R, BALL_R);
+        });
 
+
+        //GAME OVER SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //create the window widgets of the GAME OVER SCREEN
+        Button quit2 = new Button("QUIT");
+        quit2.setLayoutX(WIDTH - 50);
+        quit2.setLayoutY(10);
+
+        Button restart = new Button("Play Again");
+        restart.setLayoutY(10);
+
+        Text gameOver = new Text("GAME OVER !");
+        gameOver.setX(WIDTH / 2 - 300);
+        gameOver.setY(HEIGHT / 2 - 100);
+        gameOver.setFont((Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR, 80)));
+
+        Label enterLabel = new Label("Please enter your name to add yourself to the scoreboard!");
+
+        Text finalScoreText = new Text("Your score: ");
+
+
+        HBox finalScoresHBox = new HBox();
+        finalScoresHBox.setLayoutX(WIDTH / 2 - 75);
+        finalScoresHBox.setLayoutY(HEIGHT / 2 - 50);
+
+        finalScoresHBox.getChildren().addAll(finalScoreText, finalScore);
+
+        TextField enterName = new TextField();
+
+        Button enter = new Button("ENTER");
+
+        VBox enterNameArea = new VBox();
+        enterNameArea.setLayoutX(WIDTH / 2 - 180);
+        enterNameArea.setLayoutY(HEIGHT / 2);
+
+        // Create two columns
+        TableColumn<String, Players> cl1 = new TableColumn<>("Player Name");
+        cl1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Integer, Players> cl2 = new TableColumn<>("Player Score");
+        cl2.setCellValueFactory(new PropertyValueFactory<>("score"));
+
+        // Add two columns into TableView
+        tbv.getColumns().add(cl1);
+        tbv.getColumns().add(cl2);
+
+        //Defines the pane hierarchy for the GAME OVER SCREEN
+        enterNameArea.getChildren().addAll(enterLabel, enterName, enter, tbv);
+        tbv.setVisible(false);
+        p3.getChildren().addAll(quit2, restart, gameOver, enterNameArea, finalScoresHBox);
+
+        //Event listeners for GAME OVER SCREEN
+        enterName.textProperty().addListener(e -> {
+            //  enter.setDisable(false);
+        });
+        enter.setOnMouseClicked(e -> {
+            addScores(new Players(enterName.getText(), playerScore));
+            System.out.println(scoreBoard);
+            updateBoard();
+            tbv.setVisible(true);
+            enterName.setText("");
+            enter.setDisable(true);
+
+        });
+        quit2.setOnMouseClicked(e -> {
+            enter.setDisable(false);
+            resetGame();
+            primaryStage.setScene(welcomeScreen);
+
+        });
+        restart.setOnMouseClicked(e -> {
+            enter.setDisable(false);
+            resetGame();
+            primaryStage.setScene(gameScreen);
+            ptBall.play();
+        });
+
+        // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //shows the window of the application, beginning with the WELCOME SCREEN
+        primaryStage.setTitle("P O N G");
+        primaryStage.setScene(welcomeScreen);
+        primaryStage.show();
+
+    }
+
+    /**
+     * Resets all variables and positions to default for game start
+     */
+    public void resetGame() {
+        //Sets score back to zero
+        playerScore = 0;
+        score.setText("" + playerScore);
+
+        //Resets lives
+        lives = 3;
+        currentLives.setText("" + lives);
+
+        //Resets final score
+        finalScore.setText("" + playerScore);
+
+        //Reset Rate
+        ptBall.setRate(1.0);
+
+        //Clears scorebaord and hides it
+        tbv.setVisible(false);
+        tbv.getItems().clear();
+
+
+    }
+
+    /**
+     * Starts the game and defines the game logic.
+     */
+    public void gamePlay() {
+        //BALL ANIMATION
+        ptBall.setDuration(javafx.util.Duration.seconds(3));
+        Line ballPath = new Line(700, HEIGHT, 15, 100);
+
+        ptBall.setPath(ballPath);//start going left
+        ptBall.setNode(ball);
+        ptBall.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        ptBall.setInterpolator(Interpolator.LINEAR);
+        ptBall.setCycleCount(1);
+        ptBall.setAutoReverse(false);
+
+        ptBall.setOnFinished(e -> {
+            //if hits the battle, reverse:
+            if (ball.getTranslateX() <= 15) {
+                if ((humanPaddle.getY() <= ball.getTranslateY() && ball.getTranslateY() <= (humanPaddle.getY() + PLAYER_HEIGHT))) {
+                    playerScore++;
+                    score.setText("" + playerScore);
+                    ptBall.setRate(ptBall.getRate() * -1);
+                    if (ptBall.getRate() > 0) {
+                        ptBall.setRate(ptBall.getRate() + 0.5);
+                    } else {
+                        ptBall.setRate(ptBall.getRate() - 0.5);
+                    }
+
+                    ptBall.play();
+
+                } else {
+                    if (lives > 1) {
+                        lives--;
+                        currentLives.setText("" + lives);
+                        ptBall.setRate(ptBall.getRate() * -1);
+                        if (ptBall.getRate() > 0) {
+                            ptBall.setRate(ptBall.getRate() + 0.5);
+                        } else {
+                            ptBall.setRate(ptBall.getRate() - 0.5);
+                        }
+                        ptBall.play();
+                    } else {
+                        primaryStage.setScene(overScreen);
+                        finalScore.setText("" + playerScore);
+                        ptBall.stop();
+                    }
+
+                }
+
+            } else {
+                if (ball.getTranslateX() >= 785) {
+                    if ((computerPaddle.getY() <= ball.getTranslateY() && ball.getTranslateY() <= (computerPaddle.getY() + PLAYER_HEIGHT))) {
+                        ptBall.setRate(ptBall.getRate() * -1);
+                        ptBall.play();
+
+                    } else {//else, lose a point / Gameover because you missed
+                        primaryStage.setScene(overScreen);
+                        finalScore.setText("" + playerScore);
+                        ptBall.stop();
+
+                    }
+                }
+            }
+
+        });
+        ptBall.play();
+
+    }
+
+    /**
+     * Arranges the best scores of all the players and their corresponding names ordered from highest to lowest scores
+     */
+    public static void addScores(Players p) {
+        String name;
+        double gpa;
+        int index, ind = 0;
+
+        if (scoreBoard.isEmpty()) {
+            scoreBoard.add(new Players(p.getName(), p.getScore()));
         } else {
-            //set the start text
-            gc.setStroke(Color.WHITE);
-            gc.setTextAlign(TextAlignment.CENTER);
-            gc.strokeText("Click", width / 2, height / 2);
-
-            //reset the ball start position
-            ballXPos = width / 2;
-            ballYPos = height / 2;
-
-            //reset the ball speed and the direction
-            ballXSpeed = new Random().nextInt(2) == 0 ? 1: -1;
-            ballYSpeed = new Random().nextInt(2) == 0 ? 1: -1;
+            index = scoreBoard.size();
+            if (p.getScore() > scoreBoard.get(index - 1).getScore()) {
+                index--;
+                while (index > 0) {
+                    if (p.getScore() > scoreBoard.get(index - 1).getScore()) {
+                        index--;
+                    } else {
+                        ind = index;
+                        index = 0;
+                    }
+                    ind = index;
+                }
+                scoreBoard.add(ind, new Players(p.getName(), p.getScore()));
+            } else {
+                scoreBoard.add(ind, new Players(p.getName(), p.getScore()));
+            }
         }
-
-        //makes sure the ball stays in the canvas
-        if(ballYPos > height || ballYPos < 0) ballYSpeed *=-1;
-
-        //if you miss the ball, computer gets a point
-        if(ballXPos < playerOneXPos - PLAYER_WIDTH) {
-            scoreP2++;
-            gameStarted = false;
-        }
-
-        //if the computer misses the ball, you get a point
-        if(ballXPos > playerTwoXPos + PLAYER_WIDTH) {
-            scoreP1++;
-            gameStarted = false;
-        }
-
-        //increase the speed after the ball hits the player
-        if( ((ballXPos + BALL_R > playerTwoXPos) && ballYPos >= playerTwoYPos && ballYPos <= playerTwoYPos + PLAYER_HEIGHT) ||
-                ((ballXPos < playerOneXPos + PLAYER_WIDTH) && ballYPos >= playerOneYPos && ballYPos <= playerOneYPos + PLAYER_HEIGHT)) {
-            ballYSpeed += 1 * Math.signum(ballYSpeed);
-            ballXSpeed += 1 * Math.signum(ballXSpeed);
-            ballXSpeed *= -1;
-            ballYSpeed *= -1;
-        }
-
-        //draw score
-        gc.fillText(scoreP1 + "\t\t\t\t\t\t\t\t" + scoreP2, width / 2, 100);
-        //draw player 1 & 2
-        gc.fillRect(playerTwoXPos, playerTwoYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
-        gc.fillRect(playerOneXPos, playerOneYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
     }
 
-    // start the application
-    public static void main(String[] args) {
-        launch(args);
+    /**
+     * Updates the scoreboard
+     */
+    public void updateBoard() {
+        //clears the table so that names are not doubled
+        tbv.getItems().clear();
+
+        // Load objects into table
+        for (Players p : scoreBoard) {
+            tbv.getItems().add(p);
+        }
     }
+
 }
+
+
+
