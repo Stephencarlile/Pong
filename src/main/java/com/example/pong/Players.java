667af2edcb542af8336452162615b32e234f82b8
NColu20020 <98605883+NColu20020@users.noreply.github.com>
2022-04-22 15:23:26 +0200

    new stuff

---
+++ src/main/java/com/example/pong/Players.java
@@ -0,0 +1,37 @@
+package com.example.pong;
+
+public class Players {
+
+    private String name;
+    private int score;
+
+
+    public Players(String name, int score){
+        this.name=name;
+        this.score=score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Players{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
--- src/main/java/com/example/pong/PongV2.java
+++ src/main/java/com/example/pong/PongV2.java
@@ -1,24 +1,25 @@
 package com.example.pong;
 
 import javafx.animation.Interpolator;
-import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
-import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.Window;
-import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -42,7 +43,7 @@ public class PongV2 extends Application {
     private double computerXPos = WIDTH - PLAYER_WIDTH;
     private double playerYPos = WIDTH / 2;
     private double computerYPos = WIDTH / 2;
-
+    private static List<Players> scoreBoard = new ArrayList<Players>();//arrayList to store scores for scoreboards
 
     //Global node declarations
     //create the window widgets of the GAME SCREEN
@@ -50,9 +51,9 @@ public class PongV2 extends Application {
     Rectangle computerPaddle = new Rectangle(785, (HEIGHT / 2 - 75), PLAYER_WIDTH, PLAYER_HEIGHT);
     Circle ball = new Circle(0, 0, BALL_RADIUS);
 
-    Label score = new Label(""+playerScore);
+    Label score = new Label("" + playerScore);
 
-    Label currentLives = new Label(""+lives);
+    Label currentLives = new Label("" + lives);
 
     Random rand = new Random();
 
@@ -104,32 +105,33 @@ public class PongV2 extends Application {
         //GAME SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
         //create the window widgets of the GAME SCREEN
         Button quit = new Button("QUIT");
-        quit.setLayoutX(WIDTH-50);
+        quit.setLayoutX(WIDTH - 50);
         quit.setLayoutY(10);
 
         Text scoreLabel = new Text("Player Score:");
-        scoreLabel.setLayoutX(WIDTH/2-30);
+        scoreLabel.setLayoutX(WIDTH / 2 - 30);
         scoreLabel.setLayoutY(12);
 
         //Score label
-        score.setLayoutX(WIDTH/2);
+        score.setLayoutX(WIDTH / 2);
         score.setLayoutY(20);
 
         Text livesLabel = new Text("Lives Left:");
-        livesLabel.setLayoutX(WIDTH/2-20);
+        livesLabel.setLayoutX(WIDTH / 2 - 20);
         livesLabel.setLayoutY(570);
 
         //Score label
-        currentLives.setLayoutX(WIDTH/2);
+        currentLives.setLayoutX(WIDTH / 2);
         currentLives.setLayoutY(580);
 
 
         // defines the pane hierarchy for the WELCOME SCREEN
-        p2.getChildren().addAll(humanPaddle, computerPaddle, ball, quit,scoreLabel,score,currentLives, livesLabel);
+        p2.getChildren().addAll(humanPaddle, computerPaddle, ball, quit, scoreLabel, score, currentLives, livesLabel);
 
         //EVENT Listeners for GAME SCREEN
         quit.setOnMouseClicked(e -> {
             primaryStage.setScene(overScreen);
+            ptBall.stop();
         });
 
 //        gameScreen.setOnKeyPressed(e -> {
@@ -151,7 +153,7 @@ public class PongV2 extends Application {
         ball.translateYProperty().addListener(ov ->
         {
             computerPaddle.setY((ball.getTranslateY() - PLAYER_HEIGHT / 2));
-            if (ball.getTranslateY()>=HEIGHT || ball.getTranslateY()<= 0){
+            if (ball.getTranslateY() >= HEIGHT || ball.getTranslateY() <= 0) {
                 ptBall.setRate(ptBall.getRate() * -1);
             }
         });
@@ -160,28 +162,48 @@ public class PongV2 extends Application {
         //GAME OVER SCREEN----------------------------------------------------------------------------------------------------------------------------------------------------------------------
         //create the window widgets of the GAME OVER SCREEN
         Button quit2 = new Button("QUIT");
-        quit2.setLayoutX(WIDTH-50);
-        quit2.setLayoutY(10);Button restart = new Button("RESTART");
+        quit2.setLayoutX(WIDTH - 50);
+        quit2.setLayoutY(10);
+        Button restart = new Button("RESTART");
 
         Text gameOver = new Text("GAME OVER !");
-        gameOver.setX(WIDTH/2 -300);
-        gameOver.setY(HEIGHT/2);
-        gameOver.setFont((Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR,80)));
+        gameOver.setX(WIDTH / 2 - 300);
+        gameOver.setY(HEIGHT / 2);
+        gameOver.setFont((Font.font("Verdana", FontWeight.EXTRA_BOLD, FontPosture.REGULAR, 80)));
 
         Label enterLabel = new Label("Please enter your name to add yourself to the scoreboard!");
-        enterLabel.setLayoutX(WIDTH/2 -200);
-        enterLabel.setLayoutY(HEIGHT/2+50);
 
         TextField enterName = new TextField();
-        enterName.setLayoutX(WIDTH/2-110);
-        enterName.setLayoutY(HEIGHT/2+70);
+        // enterName.setDisable(false);
 
+        Button enter = new Button("ENTER");
+        //  enter.setDisable(true);
+
+        VBox enterNameArea = new VBox();
+        enterNameArea.setLayoutX(WIDTH / 2 - 180);
+        enterNameArea.setLayoutY(HEIGHT / 2 + 40);
 
         //Defines the pane hierarchy for the GAME OVER SCREEN
-        p3.getChildren().addAll(quit2, restart, gameOver,enterName,enterLabel);
+        enterNameArea.getChildren().addAll(enterLabel, enterName, enter);
+        p3.getChildren().addAll(quit2, restart, gameOver, enterNameArea);
 
         //Event listeners for GAME OVER SCREEN
+        enterName.textProperty().addListener(e -> {
+            //  enter.setDisable(false);
+        });
+        enter.setOnMouseClicked(e -> {
+            //  enterName.setDisable(true);
+
+            addScores(new Players(enterName.getText(), playerScore));
+            System.out.println(scoreBoard);
+
+            TableView table = new TableView();
+            table = displayScore();
+            enterNameArea.getChildren().add(table);
+
+        });
         quit2.setOnMouseClicked(e -> {
+           // enterNameArea.getChildren().remove(table);
             primaryStage.setScene(welcomeScreen);
             resetGame();
         });
@@ -189,7 +211,6 @@ public class PongV2 extends Application {
             resetGame();
             primaryStage.setScene(gameScreen);
             ptBall.play();
-
         });
 
         // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -208,11 +229,11 @@ public class PongV2 extends Application {
 
         //Sets score back to zero
         playerScore = 0;
-        score.setText(""+playerScore);
+        score.setText("" + playerScore);
 
         //Resets lives
         lives = 3;
-        currentLives.setText(""+lives);
+        currentLives.setText("" + lives);
 
         //Reset Rate
         ptBall.setRate(1.0);
@@ -238,31 +259,28 @@ public class PongV2 extends Application {
             if (ball.getTranslateX() <= 15) {
                 if ((humanPaddle.getY() <= ball.getTranslateY() && ball.getTranslateY() <= (humanPaddle.getY() + PLAYER_HEIGHT))) {
                     playerScore++;
-                    score.setText(""+playerScore);
+                    score.setText("" + playerScore);
                     ptBall.setRate(ptBall.getRate() * -1);
-                    if (ptBall.getRate()>0){
+                    if (ptBall.getRate() > 0) {
                         ptBall.setRate(ptBall.getRate() + 0.5);
-                    }
-                    else{
+                    } else {
                         ptBall.setRate(ptBall.getRate() - 0.5);
                     }
 
                     ptBall.play();
 
                 } else {
-                    if(lives>1){
+                    if (lives > 1) {
                         lives--;
-                        currentLives.setText(""+lives);
+                        currentLives.setText("" + lives);
                         ptBall.setRate(ptBall.getRate() * -1);
-                        if (ptBall.getRate()>0){
+                        if (ptBall.getRate() > 0) {
                             ptBall.setRate(ptBall.getRate() + 0.5);
-                        }
-                        else{
+                        } else {
                             ptBall.setRate(ptBall.getRate() - 0.5);
                         }
                         ptBall.play();
-                    }
-                    else {
+                    } else {
                         primaryStage.setScene(overScreen);
                         ptBall.stop();
                     }
@@ -285,8 +303,62 @@ public class PongV2 extends Application {
 
         });
         ptBall.play();
-        //System.out.println(ptBall.getRate());
-
 
     }
+
+    /**
+     * Arranges the best scores of all the players and their corresponding names
+     */
+    public static void addScores(Players p) {
+        String name;
+        double gpa;
+        int index, ind = 0;
+
+        if (scoreBoard.isEmpty()) {
+            scoreBoard.add(new Players(p.getName(), p.getScore()));
+        } else {
+            index = scoreBoard.size();
+            if (p.getScore() > scoreBoard.get(index - 1).getScore()) {
+                index--;
+                while (index > 0) {
+                    if (p.getScore() > scoreBoard.get(index - 1).getScore()) {
+                        index--;
+                    } else {
+                        ind = index;
+                        index = 0;
+                    }
+                    ind = index;
+                }
+                scoreBoard.add(ind, new Players(p.getName(), p.getScore()));
+            } else {
+                scoreBoard.add(ind, new Players(p.getName(), p.getScore()));
+            }
+        }
+    }
+
+    public TableView displayScore() {
+        // New Table View
+        TableView tbv = new TableView();
+
+        // Create two columns
+        TableColumn<String, Players> cl1 = new TableColumn<>("Player Name");
+        cl1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Integer, Players> cl2 = new TableColumn<>("Player Score");
+        cl2.setCellValueFactory(new PropertyValueFactory<>("score"));
+
+        // Add two columns into TableView
+        tbv.getColumns().add(cl1);
+        tbv.getColumns().add(cl2);
+
+        // Load objects into table
+        for (Players p : scoreBoard){
+            tbv.getItems().add(p);
+        }
+
+        return tbv;
+    }
+
 }
+
+
+
